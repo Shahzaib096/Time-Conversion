dcade8bb7e744733082e41448e2ac7ae600ce280
Shahzaib Hassan <61833621+Shahzaib096@users.noreply.github.com>
2020-03-11 15:10:53 +0500

    Add files via upload

---
+++ Time Conversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Time You Want To Convert :");
+            DateTime d = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Converted Time iS :" + d.ToString("HH:mm::ss"));
+            Console.ReadLine();
+        }
+    }
+}
